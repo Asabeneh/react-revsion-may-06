06d6faffcaec68464b616dc767fa285c72d042a0
Asabeneh <asabeneh@gmail.com>
2020-03-02 16:52:00 +0200

    some changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,10 +10,10 @@ const Main = () => {
             <Layout>Section two layout</Layout>
             <Layout>Section three layout</Layout>
             <Header
-                title="Everyobdy is enjoying React"
+                title="Everybody is enjoying React"
                 year={new Date().getFullYear()}
             >
-                I can add text in here and I will be accessed as a child propps
+                I can add text in here and it will be accessed as a child props
       </Header>
         </main>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,52 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 const Navbar = props => {
-  const names = ['John', 'David', 'Brook', 'Asab'];
-  const index = Math.floor(Math.random() * names.length);
-  const name = names[index].toLowerCase();
+  const names = ['John', 'David', 'Brook', 'Asab']
+  const index = Math.floor(Math.random() * names.length)
+  const name = names[index].toLowerCase()
   return (
     <div>
-          <ul>
-              <li>
-                  <NavLink to="/" exact>
-                      HOME
-        </NavLink>
-              </li>
-              <li>
-                  <NavLink to="/about">ABOUT</NavLink>
-              </li>
-              <li>
-                  <NavLink to="/contact">CONTACT</NavLink>
-              </li>
-              <li>
-                  <NavLink to="/topics">TOPICS</NavLink>
-              </li>
-              <li>
-                  <NavLink to="/add-user">Add User</NavLink>
-              </li>
-              <li>
-                  <NavLink to="/add-todo">Add Todo</NavLink>
-              </li>
-              <li>
-                  <NavLink to="/todos">Todo List</NavLink>
-              </li>
-              <li>
-                  <NavLink to="/countries">Countries</NavLink>
-              </li>
-              <li>
-                  <NavLink to="/count">Count</NavLink>
-              </li>
-              <li>
-                  <NavLink to={`/user/${name}`}>User {names[index]}</NavLink>
-              </li>
-          </ul>
-          <hr />
+      <ul>
+        <li>
+          <NavLink to='/' exact>
+            HOME
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/about'>ABOUT</NavLink>
+        </li>
+        <li>
+          <NavLink to='/contact'>CONTACT</NavLink>
+        </li>
+        <li>
+          <NavLink to='/topics'>TOPICS</NavLink>
+        </li>
+        <li>
+          <NavLink to='/add-user'>Add User</NavLink>
+        </li>
+        <li>
+          <NavLink to='/add-todo'>Add Todo</NavLink>
+        </li>
+        <li>
+          <NavLink to='/todos'>Todo List</NavLink>
+        </li>
+        <li>
+          <NavLink to='/countries'>Countries</NavLink>
+        </li>
+        <li>
+          <NavLink to='/count'>Count</NavLink>
+        </li>
+        <li>
+          <NavLink to={`/user/${name}`}>User {names[index]}</NavLink>
+        </li>
+      </ul>
+      <hr />
     </div>
-    
-  );
-};
+  )
+}
 
-Navbar.propTypes = {};
+Navbar.propTypes = {}
 
-export default Navbar;
+export default Navbar
